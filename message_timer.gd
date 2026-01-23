ce9a02f2b55ec8056f62311fdb0477a817888b74
hjmunoz <hector.jefrey@gmail.com>
2026-01-23 06:51:49 -0500

    se agregan funcionalidades en la parte de HUB UI

--- message_timer.gd
+++ message_timer.gd
@@ -14,3 +14,26 @@ func show_message(text):
 	%Message.text = text
 	%Message.show()
 	%MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+
+
+
+func _on_start_button_pressed() -> void:
+	$StarButton.hide()
+	start_game.emit()
+
+
+func _on_timeout() -> void:
+	$Message.hide()
